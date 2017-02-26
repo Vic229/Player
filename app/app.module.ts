dc6d0219d15f75ca1312bcc84da2eea750bd2d84
Vic229 <vic229@rambler.ru>
2017-02-26 18:07:05 +0300

    Revert "Revert "Song component with button""
    This reverts commit 2640e944627bb736fac99e57d68a284ad68c4909.

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
+import { SongComponent }  from './Songs/song.component';
 
 @NgModule({
   imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent, SongComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
